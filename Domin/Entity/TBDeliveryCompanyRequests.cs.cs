61bb39f00f50670c0fffa334e5af3b9083025f2f
nohad darweish <nohadking@hotmail.com>
2025-03-08 19:44:18 +0300

    update requst

---
+++ Domin/Entity/TBDeliveryCompanyRequests.cs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domin.Entity
+{
+	public class TBDeliveryCompanyRequests
+	{
+		[Key]
+        public int IdDeliveryCompanyRequests { get; set; }
+		public int IdDeliveryCompanie { get; set; }
+		[Required(ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "VlNameClint")]
+		[MaxLength(300, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MaxLength300")]
+		[MinLength(3, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MinLength3")]
+		public string NameClint { get; set; }
+		[Required(ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "VlPhonenumber")]
+		[MaxLength(20, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MaxLength20")]
+		[MinLength(3, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MinLength3")]
+		public string PhoneNumber { get; set; }
+		public int IdDeliveryCompanyPricing { get; set; }
+		[Required(ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "VlFullAddress")]
+		[MaxLength(500, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MaxLength500")]
+		[MinLength(3, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MinLength3")]
+		public string FullAddress { get; set; }
+		[MaxLength(2000, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MaxLength2000")]
+		[MinLength(3, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MinLength3")]
+		public string? Nouts { get; set; }
+
+		public int InvoiceNumber { get; set; }
+		public DateTime DateTimeInvose { get; set; }
+		public int IdPaymentMethod { get; set; }
+		public int IdTypeOrder { get; set; }
+		public string DataEntry { get; set; }
+		public DateTime DateTimeEntry { get; set; }
+
+		public bool CurrentState { get; set; }
+
+
+
+
+
+
+
+	}
+}
---
+++ Infarstuructre/Migrations/20250308132906_TBDeliveryCompanyRequests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+#nullable disable
+namespace Infarstuructre.Migrations
+{
+    /// <inheritdoc />
+    public partial class TBDeliveryCompanyRequests : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBDeliveryCompanyRequestss",
+                columns: table => new
+                {
+                    IdDeliveryCompanyRequests = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdDeliveryCompanie = table.Column<int>(type: "int", nullable: false),
+                    NameClint = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    IdDeliveryCompanyPricing = table.Column<int>(type: "int", nullable: false),
+                    FullAddress = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Nouts = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: true),
+                    InvoiceNumber = table.Column<int>(type: "int", nullable: false),
+                    DateTimeInvose = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IdPaymentMethod = table.Column<int>(type: "int", nullable: false),
+                    IdTypeOrder = table.Column<int>(type: "int", nullable: false),
+                    DataEntry = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateTimeEntry = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "getdate()"),
+                    CurrentState = table.Column<bool>(type: "bit", nullable: false, defaultValueSql: "((1))")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBDeliveryCompanyRequestss", x => x.IdDeliveryCompanyRequests);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBDeliveryCompanyRequestss");
+        }
+    }
+}
